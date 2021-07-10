9b31efabb936a3ca02d22b75dd70c1c9acc1a364
yasuyuki0422 <xiangchuanjingxing04@gmail.com>
2021-07-10 10:47:05 +0900

    first commit

---
+++ kanban/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = "kanban"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("home/", views.home, name="home"),
+    path("signup/", views.signup, name="signup"),
+    path("users/<int:pk>/", views.UserDetailView.as_view(), name="users_detail"),
+    path("users/<int:pk>/update/", views.UserUpdateView.as_view(), name="users_update"), # 追加
+]
---
+++ kanban/views.py
@@ -0,0 +1,46 @@
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, resolve_url
+from django.views.generic import DetailView, UpdateView
+
+from .forms import UserForm
+from .mixins import OnlyYouMixin # 忘れずにインポートする
+
+def index(request):
+    return render(request, "kanban/index.html")
+
+
+@login_required
+def home(request):
+    return render(request, "kanban/home.html")
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("kanban:home")
+    else:
+        form = UserCreationForm()
+        context = {
+            "form": form
+        }
+    return render(request, 'kanban/signup.html', context)
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = "kanban/users/detail.html"
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = "kanban/users/update.html"
+    form_class = UserForm
+
+    def get_success_url(self):
+        return resolve_url('kanban:users_detail', pk=self.kwargs['pk'])
